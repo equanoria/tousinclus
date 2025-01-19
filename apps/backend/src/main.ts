b280ee55104d3e4878521d56a766052e3ef1153b
Dan PN <77532827+Derlvx@users.noreply.github.com>
2025-01-19 14:16:20 +0100

    feat(TASK-106): :sparkles: add api to all route in the backend
    use "setGlobalPrefix"

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Ajouter un préfixe global à toutes les routes
+  app.setGlobalPrefix('api');
+
   const port = parseInt(process.env.PORT, 10) || 3001;
   await app.listen(port);
 }
