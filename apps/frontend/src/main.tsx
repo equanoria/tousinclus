2d878828048223e63640a20369f297ca1dcce247
Léo PLANUS <48881747+leo29plns@users.noreply.github.com>
2025-03-12 14:40:23 +0100

    cleaning

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
-// biome-ignore lint/style/noNonNullAssertion: <explanation>
+// biome-ignore lint/style/noNonNullAssertion: vite
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
