83a67d19cf8e952eb7ea686fc9204d94cd67426b
Léo PLANUS <48881747+leo29plns@users.noreply.github.com>
2025-03-13 10:22:22 +0100

    fix(TASK-171): fix IDs

--- apps/frontend/src/components/Checkbox/Checkbox.tsx
+++ apps/frontend/src/components/Checkbox/Checkbox.tsx
@@ -1,20 +1,19 @@
 import clsx from 'clsx';
 import type React from 'react';
-import type { ComponentPropsWithoutRef } from 'react';
+import { useId, type ComponentPropsWithoutRef } from 'react';
 import styles from './Checkbox.module.css';
 
 export interface CheckboxProps extends ComponentPropsWithoutRef<'input'> {
   label: string;
-  id: string;
 }
 
 export const Checkbox: React.FC<CheckboxProps> = ({
   className,
   label,
-  id,
   ...props
 }) => {
   const classes = clsx(styles.checkbox, className);
+  const id = useId();
 
   return (
     <div className={classes}>
--- apps/frontend/src/components/Input/Input.tsx
+++ apps/frontend/src/components/Input/Input.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx';
 import type React from 'react';
-import type { ComponentPropsWithoutRef } from 'react';
+import { useId, type ComponentPropsWithoutRef } from 'react';
 import styles from './Input.module.css';
 
 export interface InputProps extends ComponentPropsWithoutRef<'input'> {
@@ -8,19 +8,18 @@ export interface InputProps extends ComponentPropsWithoutRef<'input'> {
   placeholder: string;
 }
 
-
-
 export const Input: React.FC<InputProps> = ({
   className,
   label,
   placeholder,
 }) => {
   const classes = clsx(styles.blocInput, className);
+  const id = useId(); 
 
   return (
     <div className={classes}>
-      <label className={styles.label} htmlFor="">{label}</label>
-      <input type="text" className={styles.input} placeholder={placeholder} />
+      <label className={styles.label} htmlFor={id}>{label}</label>
+      <input type="text" id={id} className={styles.input} placeholder={placeholder} />
     </div>
 
   );
