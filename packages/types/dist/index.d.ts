81e1be4a027d13423e0055a2c6faa2f40effb22c
Léo PLANUS <48881747+leo29plns@users.noreply.github.com>
2025-03-14 10:05:15 +0100

    fix(TASK-205): commonjs package

--- packages/types/dist/index.d.ts
+++ packages/types/dist/index.d.ts
@@ -2,5 +2,9 @@ export * from './TLanguage';
 export * from './ITranslation';
 export * from './IDirectus';
 export * from './IGame';
+export * from './IUser';
 export * from './IWebsocket';
+export * from './TRole';
+export * from './TTeam';
+export * from './TWaiting';
 //# sourceMappingURL=index.d.ts.map
