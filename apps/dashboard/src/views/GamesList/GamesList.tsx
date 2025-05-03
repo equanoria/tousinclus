bec3e9a0cf68ff0f9555331227ea58f8450f0322
Léo PLANUS <48881747+leo29plns@users.noreply.github.com>
2025-05-03 17:11:46 +0200

    chore(TASK-151): add tailwind

--- apps/dashboard/src/views/GamesList/GamesList.tsx
+++ apps/dashboard/src/views/GamesList/GamesList.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import styles from './GamesList.module.css';
 
 export const GamesList = () => {
@@ -5,6 +6,8 @@ export const GamesList = () => {
     <div className={styles.gamesList}>
       <h1>Dashboard</h1>
       <h2>Parties en cours</h2>
+
+      <Button>Je suis un bouton shadcn/ui</Button>
     </div>
   );
 };
