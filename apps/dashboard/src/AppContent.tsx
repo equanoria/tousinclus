9b0130b2194f3d545b4c3fdc74578ed47e7d6df5
Léo PLANUS <48881747+leo29plns@users.noreply.github.com>
2025-05-03 15:34:30 +0200

    feat(TASK-151): create Dashboard app

---
+++ apps/dashboard/src/AppContent.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+import { useAppState } from './context/AppStateProvider';
+import { GamesList } from './views/GamesList/GamesList';
+
+const AppContent = () => {
+  const { currentView, setCurrentView } = useAppState();
+
+  useEffect(() => {
+    setCurrentView(<GamesList />);
+  }, [setCurrentView]);
+
+  return (
+    <>
+      <nav className="a11y-skip-content">
+        <ul>
+          <li>
+            <a href="#main">Aller au contenu principal</a>
+          </li>
+          <li>
+            <a href="#main">Aller à un autre endroit</a>
+          </li>
+        </ul>
+      </nav>
+      <main id="main">{currentView}</main>
+    </>
+  );
+};
+
+export default AppContent;
---
+++ apps/dashboard/src/services/DirectusService.ts
@@ -0,0 +1,28 @@
+import { createDirectus, readItems, rest } from '@directus/sdk';
+import type { TLanguage } from '@tousinclus/types';
+import { isValidUrl } from '../utils/isValidUrl';
+
+interface Schema {
+  languages: TLanguage[];
+}
+
+export class DirectusService {
+  private directus;
+
+  public constructor() {
+    const directusBaseUrl = isValidUrl(window.env.DIRECTUS_URL)
+    ? window.env.DIRECTUS_URL
+    : 'http://127.0.0.1:3002';
+
+    this.directus = createDirectus<Schema>(directusBaseUrl).with(rest())
+  }
+
+  public async getLanguages(): Promise<TLanguage[]> {
+    try {
+      return await this.directus.request(readItems('languages'));
+    } catch (error) {
+      console.error('Cannot get Languages from Directus.', error);
+      return [];
+    }
+  }
+}
