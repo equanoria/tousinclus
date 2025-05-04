e5c3a1bd5994fbe94a39332823572f0c8001b3e3
Léo PLANUS <48881747+leo29plns@users.noreply.github.com>
2025-05-04 13:14:34 +0200

    chore(TASK-278): remove shadcn/ui

--- apps/dashboard/src/views/GamesList/GamesList.tsx
+++ apps/dashboard/src/views/GamesList/GamesList.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@/components/ui/button';
 import styles from './GamesList.module.css';
 
 export const GamesList = () => {
@@ -7,7 +6,7 @@ export const GamesList = () => {
       <h1>Dashboard</h1>
       <h2>Parties en cours</h2>
 
-      <Button>Je suis un bouton shadcn/ui</Button>
+      Je suis un bouton
     </div>
   );
 };
--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -1,20 +1,12 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
-import path from 'node:path';
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   server: {
     port: 3006,
     strictPort: true,
-  },
-
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
   },
 });
