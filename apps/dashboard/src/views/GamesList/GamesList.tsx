39fe5b0fef62a96e34718e94a901843d4205f3d8
Léo PLANUS <48881747+leo29plns@users.noreply.github.com>
2025-05-04 14:02:56 +0200

    chore(TASK-278): Directus as service

--- apps/dashboard/src/views/GamesList/GamesList.tsx
+++ apps/dashboard/src/views/GamesList/GamesList.tsx
@@ -1,12 +1,66 @@
+import { useAppState } from '../../context/AppStateProvider';
 import styles from './GamesList.module.css';
+import {
+  Button,
+  Form,
+  Input,
+} from 'antd';
+
+const messages = {
+  email: 'Veuillez entrer une adresse e-mail valide',
+  password: 'Veuillez entrer votre mot de passe',
+};
 
 export const GamesList = () => {
-  return (
-    <div className={styles.gamesList}>
-      <h1>Dashboard</h1>
-      <h2>Parties en cours</h2>
+  const { directusService } = useAppState();
+  const [form] = Form.useForm();
 
-      Je suis un bouton
-    </div>
+  const onFinish = (values: unknown) => {
+    console.log('Received values of form: ', values);
+  };
+
+  return (
+    <Form
+      form={form}
+      name="register"
+      onFinish={onFinish}
+      scrollToFirstError
+    >
+      <Form.Item
+        name="email"
+        label="E-mail"
+        rules={[
+          {
+            type: 'email',
+            message: messages.email,
+          },
+          {
+            required: true,
+            message: messages.email,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="password"
+        label="Password"
+        rules={[
+          {
+            required: true,
+            message: messages.password,
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Log In
+        </Button>
+      </Form.Item>
+    </Form>
   );
 };
