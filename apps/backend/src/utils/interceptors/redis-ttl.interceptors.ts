29ddd4356c5973d7cb7199425355658970122e4b
Dan PN <77532827+Derlvx@users.noreply.github.com>
2025-05-14 16:42:38 +0200

    feat(TASK-283): :sparkles: add TTL to redis data

---
+++ apps/backend/src/utils/interceptors/redis-ttl.interceptors.ts
@@ -0,0 +1,46 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable, from } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { RedisService } from '../../redis/redis.service';
+import { WSDataDTO } from 'src/websocket/dto/websocket.dto';
+
+@Injectable()
+export class RedisTtlInterceptor implements NestInterceptor {
+  private readonly ttlSeconds = 60 * 60 * 3;
+
+  constructor(private readonly redisService: RedisService) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    // Vérifier qu'on est bien dans un contexte WebSocket
+    const ctxType = context.getType<'ws' | 'rpc' | 'http'>();
+    if (ctxType === 'ws') {
+      const wsCtx = context.switchToWs();
+      const data = wsCtx.getData();
+      // Extraction du code depuis le payload
+      const code: WSDataDTO['code'] = data.code;
+      if (code) {
+        const key = code; // Use the game code to construct the Redis key
+        // from() transforms the Promise into an Observable
+        return from(this.redisService.getGame(code)).pipe(
+          switchMap((game) => {
+            if (game) {
+              return from(
+                this.redisService.setTTL(key, this.ttlSeconds), // Set TTL to 3 hours
+              );
+            }
+            return from(Promise.resolve(null));
+          }),
+          // Once the TTL is refreshed (or not), pass the WebSocket request to the next handler
+          switchMap(() => next.handle()),
+        );
+      }
+    }
+    // Si pas WS ou pas d'ID, on passe directement au handler
+    return next.handle();
+  }
+}
