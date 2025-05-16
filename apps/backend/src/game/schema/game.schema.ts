bcf73ce490151e5fecc74f3372730390bae412c9
Dan PN <77532827+Derlvx@users.noreply.github.com>
2025-05-16 21:43:27 +0200

    feat(TASK-249): :sparkles: first commit for game schema

---
+++ apps/backend/src/game/schema/game.schema.ts
@@ -0,0 +1,61 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ETeam, IAnswer, IAnswerData, IGame, IVote } from '@tousinclus/types';
+import { Document } from 'mongoose';
+
+type IGameMongo = Omit<IGame, 'status' | 'team1' | 'team2'>;
+
+@Schema()
+export class AnswerData implements IAnswerData {
+  @Prop({ required: true, type: String })
+  input1: string;
+
+  @Prop({ required: true, type: String })
+  input2: string;
+
+  @Prop({ required: true, type: String })
+  input3: string;
+
+  @Prop({ required: true, type: [Number] })
+  inputCheckboxes: number[];
+}
+
+@Schema()
+export class Answer implements IAnswer {
+  @Prop({ required: true, type: Number })
+  cardId: number;
+
+  @Prop({ required: true, type: ETeam })
+  team: ETeam;
+
+  @Prop({ required: true, type: AnswerData })
+  answer: AnswerData;
+}
+
+@Schema()
+export class Vote implements IVote {
+  @Prop({ required: true, type: Number })
+  cardId: number;
+
+  @Prop({ required: true, type: [ETeam] })
+  votes: { team: ETeam; vote: ETeam }[];
+}
+
+@Schema({ timestamps: true })
+export class GameDocument extends Document implements IGameMongo {
+  createdAt: Date;
+  updatedAt: Date;
+
+  @Prop({ required: true, type: String })
+  code: string;
+
+  @Prop({ required: true, type: Number })
+  cardGroupId: number;
+
+  @Prop({ required: true, type: [Answer] })
+  answers: Answer[];
+
+  @Prop({ required: true, type: [Vote] })
+  votes: Vote[];
+}
+
+export const GameSchema = SchemaFactory.createForClass(GameDocument);
