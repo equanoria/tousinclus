e27214a4de4d09b5311b0a7ae6087bddd03f39a8
Cécile <cecilephan@outlook.com>
2025-05-29 08:24:15 +0200

    feat(add): add reflection page styles

--- apps/frontend/src/views/GameReflection/GameReflection.tsx
+++ apps/frontend/src/views/GameReflection/GameReflection.tsx
@@ -8,6 +8,8 @@ import { Button } from '../../components/Button/Button';
 import { Checkbox } from '../../components/Checkbox/Checkbox';
 import { Input } from '../../components/Input/Input';
 import { Deck } from '../../components/Deck/Deck';
+import styles from './GameReflection.module.css';
+import { GameCard } from '../../components/GameCard/GameCard';
 
 const defaultAnswer = {
   input1: '',
@@ -27,25 +29,29 @@ export const GameReflection = () => {
 
   useEffect(() => {
     const fetchCardGroup = async () => {
-      const group = await directusService.getCardsGroup(gameService.cardsGroupId);
+      const group = await directusService.getCardsGroup(
+        gameService.cardsGroupId,
+      );
       setCardsGroup(group);
     };
 
-    gameReflectionService.onGetAnswersResponse(fetchCardGroup)
-  }, [])
+    gameReflectionService.onGetAnswersResponse(fetchCardGroup);
+  }, []);
 
-  const extremeUserId = cardsGroup?.extreme_user[extremeUserCursor].cards_users_id.id;
+  const usageSituation = cardsGroup?.usage_situation;
+  const extremeUserId =
+    cardsGroup?.extreme_user[extremeUserCursor].cards_users_id.id;
 
   useEffect(() => {
     if (!extremeUserId) return;
 
-    const answer = gameReflectionService.getAnswer(extremeUserId)
+    const answer = gameReflectionService.getAnswer(extremeUserId);
     if (answer?.answer) {
       setAnswerData(answer.answer);
     } else {
       setAnswerData(defaultAnswer);
     }
-  }, [extremeUserId])
+  }, [extremeUserId]);
 
   const updateCursor = (direction: 'next' | 'back') => {
     if (!cardsGroup) return;
@@ -68,9 +74,11 @@ export const GameReflection = () => {
       ...prev,
       [name]: value,
     }));
-  }
+  };
 
-  const handleCheckboxesChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleCheckboxesChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
     const { name, checked } = event.target;
     const id = Number.parseInt(name);
 
@@ -80,53 +88,123 @@ export const GameReflection = () => {
         ? [...prev.inputCheckboxes, id]
         : prev.inputCheckboxes.filter((itemId) => itemId !== id),
     }));
-  }
+  };
 
   const updateAnswer = useCallback(() => {
     if (!extremeUserId) return;
 
-    gameReflectionService.updateAnswer(
-      extremeUserId,
-      answerData,
-    );
+    gameReflectionService.updateAnswer(extremeUserId, answerData);
   }, [extremeUserId, answerData]);
 
   // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
   useEffect(() => {
     updateAnswer();
   }, [updateAnswer, answerData.inputCheckboxes]);
-  
- return (
-  <>
-    <h1>Phase de réflexion</h1>
 
-    {!hasStarted ? (
-      <>
-        <Deck cardsGroup={cardsGroup} onStart={() => setHasStarted(true)}/>
-      </>
-    ) : (
-      <>
-        <Button onClick={() => updateCursor('next')}>Next</Button>
-        <Button onClick={() => updateCursor('back')}>Back</Button>
+  return (
+    <section className={styles.pageReflection}>
+      <h1>Phase de réflexion</h1>
+      <p>
+        Trouvez des solutions pour chaque utilisateur face à la situation
+        concernée
+      </p>
+      {!hasStarted ? (
+        <>
+          <Deck cardsGroup={cardsGroup} onStart={() => setHasStarted(true)} />
+        </>
+      ) : (
+        <section className={styles.reflectionContainer}>
+          <div className={styles.leftContainer}>
+            <h2>Fiche solution</h2>
 
-        <Input label="Question 1" placeholder="Bla bla bla..." type="text" name="input1" value={answerData.input1} onChange={handleInputChange} onBlur={updateAnswer} />
-        <Input label="Question 2" placeholder="Bla bla bla..." type="text" name="input2" value={answerData.input2} onChange={handleInputChange} onBlur={updateAnswer} />
-        <Input label="Question 3" placeholder="Bla bla bla..." type="text" name="input3" value={answerData.input3} onChange={handleInputChange} onBlur={updateAnswer} />
-        
-        <fieldset>
-          {cardsGroup?.extreme_user
-            ?.filter((_, index) => index !== extremeUserCursor)
-            .map((user) => {
-              const id = user.cards_users_id.id;
-              const name = user.cards_users_id.translations[0].description;
-              return (
-                <Checkbox label={name} name={id.toString()} key={id} checked={answerData.inputCheckboxes.includes(id)} onChange={handleCheckboxesChange} />
-              );
-            })}
-        </fieldset>
-      </>
-    )}
-  </>
-);
-
-}
+            <div className={styles.containerForm}>
+              <h3>Description user</h3>
+              <div className={styles.form}>
+                <Input
+                  label="1. Description du défaut d’inclusion."
+                  placeholder="Bla bla bla..."
+                  type="text"
+                  name="input1"
+                  value={answerData.input1}
+                  onChange={handleInputChange}
+                  onBlur={updateAnswer}
+                />
+                <Input
+                  label="2. Description de la solution proposée."
+                  placeholder="Bla bla bla..."
+                  type="text"
+                  name="input2"
+                  value={answerData.input2}
+                  onChange={handleInputChange}
+                  onBlur={updateAnswer}
+                />
+                <Input
+                  label="3. Comment la solution pourrait améliorer l’expérience d’usage pour d’autres utilisateurs ?"
+                  placeholder="Bla bla bla..."
+                  type="text"
+                  name="input3"
+                  value={answerData.input3}
+                  onChange={handleInputChange}
+                  onBlur={updateAnswer}
+                />
+                <fieldset>
+                  <p>
+                    Quels sont les autres utilisateurs extrêmes concernés par la
+                    fiche réponse ?
+                  </p>
+                  {cardsGroup?.extreme_user
+                    ?.filter((_, index) => index !== extremeUserCursor)
+                    .map((user) => {
+                      const id = user.cards_users_id.id;
+                      const name =
+                        user.cards_users_id.translations[0].description;
+                      return (
+                        <Checkbox
+                          label={name}
+                          name={id.toString()}
+                          key={id}
+                          checked={answerData.inputCheckboxes.includes(id)}
+                          onChange={handleCheckboxesChange}
+                        />
+                      );
+                    })}
+                </fieldset>
+              </div>
+            </div>
+          </div>
+          <div>
+            <GameCard
+              key="situation"
+              type="situation"
+              img={directusService.getAssetUrl(usageSituation?.image)}
+              alt={
+                usageSituation?.context_translations?.[0]?.context ??
+                usageSituation?.description_translations?.[0]?.description ??
+                'Situation'
+              }
+              number={usageSituation.id}
+            />
+            {cardsGroup?.extreme_user
+              ?.filter((_, index) => index !== extremeUserCursor)
+              .map((user) => {
+                const id = user.cards_users_id.id;
+                const name = user.cards_users_id.translations[0].description;
+                const image = user.cards_users_id.image;
+                return (
+                  <GameCard
+                    key={id}
+                    type="user"
+                    img={directusService.getAssetUrl(image)}
+                    alt={name}
+                    number={id}
+                  />
+                );
+              })}
+            <Button onClick={() => updateCursor('next')}>Next</Button>
+            <Button onClick={() => updateCursor('back')}>Back</Button>
+          </div>
+        </section>
+      )}
+    </section>
+  );
+};
