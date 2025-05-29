b74246de2cb52cb8567083d0943b2b35f66fa3da
leo-planus-ext_sonepar <leo.planus_ext@sonepar.com>
2025-05-29 23:19:18 +0200

    debate getters

--- apps/frontend/src/services/game/game-debate.service.ts
+++ apps/frontend/src/services/game/game-debate.service.ts
@@ -1,7 +1,16 @@
+import type { ETeam, IAnswer } from '@tousinclus/types';
+import type { ISocketResponse } from '../../types/ISocketResponse';
 import { socketService } from '../socket/socket.service';
 import { gameService } from './game.service';
+import type { TWSResponseCallback } from './types/TWSResponseCallback';
 
 class GameDebateService {
+  private onNextVoteCallbacks: TWSResponseCallback<{ answers: IAnswer[], nextCardId: { nextCardId: number }}>[] = [];
+
+  constructor() {
+    socketService.on('debate-response', this.onGetVoteResponseDo.bind(this));
+  }
+
   getVote(): this {
     const { code } = gameService;
     socketService.emit('debate', {
@@ -12,8 +21,42 @@ class GameDebateService {
     return this;
   }
 
-  onGetVoteResponse(callback: (payload: unknown) => void): this {
-    socketService.on('debate-response', callback);
+  private onGetVoteResponseDo(payload: ISocketResponse<{ answers: IAnswer[], nextCardId: { nextCardId: number }} | IAnswer  | { nextCardId: number }>) {
+    const { status, data } = payload;
+    console.log(payload)
+
+    if (status === 'success') {
+      if ('answers' in data) {
+        for (const callback of this.onNextVoteCallbacks) {
+          callback(payload as ISocketResponse<{ answers: IAnswer[], nextCardId: { nextCardId: number }}>);
+        }
+      }
+
+      if ('nextCardId' in data && typeof data.nextCardId === 'number') {
+        this.getVote();
+      }
+    }
+  }
+
+  onNextVote(callback: TWSResponseCallback<{ answers: IAnswer[], nextCardId: { nextCardId: number }}>): this {
+    this.onNextVoteCallbacks.push(callback);
+    return this;
+  }
+
+  updateVote(cardId: number, vote: ETeam): this {
+    const { code, team } = gameService;
+    socketService.emit('debate', {
+      action: 'update-vote',
+      code,
+      data: {
+        cardId,
+        votes: [{
+          team,
+          vote,
+        }],
+      },
+    });
+
     return this;
   }
 }
