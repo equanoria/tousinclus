076a0678bfd79a75ca5916a7a6a819b80c75a5a6
Dan PN <77532827+Derlvx@users.noreply.github.com>
2025-06-07 15:58:48 +0200

    docs: :memo: add notification.controller

--- apps/backend/src/websocket/notification.controller.ts
+++ apps/backend/src/websocket/notification.controller.ts
@@ -1,6 +1,19 @@
-import { Body, Controller, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpStatus,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { WebsocketService } from './websocket.service';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { ERole } from '@tousinclus/types';
 import { GameDTO } from 'src/game/dto/game.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
@@ -15,15 +28,41 @@ export class NotificationController {
   constructor(private readonly websocketService: WebsocketService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Notify All client connected' })
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully notify all client',
+  })
+  @ApiBearerAuth('access-token')
   async notifyAll(@Body() message: string) {
     this.websocketService.emitToAll('notification', message);
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Successfully notify all client',
+    };
   }
 
   @Post(':code')
+  @ApiOperation({ summary: 'Notify All client connected' })
+  @ApiParam({
+    name: 'code',
+    example: '187543',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully notify client from room 187543',
+  })
+  @ApiBearerAuth('access-token')
   async notifyRoom(
     @Param('code') code: GameDTO['code'],
     @Body() message: string,
   ) {
     this.websocketService.emitToRoom(code, 'notification', message);
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: `Successfully notify client from room ${code}`,
+    };
   }
 }
